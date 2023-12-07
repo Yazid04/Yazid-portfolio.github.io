5adf128ec745dedec42c98f43f97cab304466e56
Yazid <yazidramadan04@gmail.com>
2023-12-07 21:45:08 +0700

    added pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Error from "./pages/Error/Error";
 
 function App() {
   return (
-   <h1 className='text-[red]'>Hello world!!!</h1>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/About" element={<About />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
   );
 }
 
